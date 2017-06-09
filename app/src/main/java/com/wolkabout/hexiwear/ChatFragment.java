4e57eb43bcaee93be694fea07ff35014eaf77f5d
MuffinMauler <NathanRobertson@dal.ca>
2017-06-09 17:30:48 -0300

    More chat stuff

--- app/src/main/java/com/wolkabout/hexiwear/ChatFragment.java
+++ app/src/main/java/com/wolkabout/hexiwear/ChatFragment.java
@@ -1,7 +1,11 @@
 package com.wolkabout.hexiwear;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,8 +23,12 @@ public class ChatFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
 
-
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_chat, container, false);
     }
 
 }
