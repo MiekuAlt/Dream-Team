3e3dbb806b37aa57f628df92e4698352cfedf231
jfinley961 <justin.finley1@gmail.com>
2017-06-13 17:54:04 -0300

    pulled files

--- app/src/main/java/com/wolkabout/hexiwear/HeartRateActivity.java
+++ app/src/main/java/com/wolkabout/hexiwear/HeartRateActivity.java
@@ -2,12 +2,16 @@ package com.wolkabout.hexiwear;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HeartRateActivity extends AppCompatActivity {
+    TextView heartText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heart_rate);
+        heartText = (TextView) findViewById(R.id.heartRate1);
+        heartText.setText("55");
     }
 }
