64ac30b762a6a96d3b45b2b5efae52a30556fe09
Michael <m.altair@live.com>
2017-06-13 12:50:56 -0300

    Brought in the GraphView API, built a line graph to be used for step count, set up the Step Count Activity, experimented on receiving Firebase data (No result so far)

--- app/src/main/java/com/wolkabout/hexiwear/activity/StepCount.java
+++ app/src/main/java/com/wolkabout/hexiwear/activity/StepCount.java
@@ -4,7 +4,7 @@ package com.wolkabout.hexiwear.activity;
  * Created by Justin on 6/8/2017.
  */
 
-class StepCount {
+public class StepCount {
     public String stepCount;
     public StepCount(String stepCount){
         this.stepCount = stepCount;
