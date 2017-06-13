1904c8fd4ae4d8afeaf5a271035557212dcd0bbf
Michael <m.altair@live.com>
2017-06-13 17:50:20 -0300

    Built the StepCounter and HeartRate functionality
    Both the StepCounter and HeartRate are pulling data from the Firebase

--- app/src/main/java/com/wolkabout/hexiwear/activity/StepCount.java
+++ app/src/main/java/com/wolkabout/hexiwear/activity/StepCount.java
@@ -5,11 +5,63 @@ package com.wolkabout.hexiwear.activity;
  */
 
 public class StepCount {
-    public String stepCount;
+    String stepCount;
+
+    public StepCount(){
+
+    }
+
     public StepCount(String stepCount){
         this.stepCount = stepCount;
     }
     public void setStepCount(String setpCount){
         this.stepCount = setpCount;
     }
+
+    public String getStepCount() {
+        return stepCount;
+    }
+
 }
+
+
+/**
+ * Created by Justin on 5/30/2017.
+ */
+
+//public class StepCount {
+//
+//    String artistId;
+//    String artistName;
+//    String artistGenre;
+//
+//    public StepCount(){
+//
+//    }
+//
+//    public StepCount(String artistId, String artistName, String artistGenre) {
+//        this.artistId = artistId;
+//        this.artistName = artistName;
+//        this.artistGenre = artistGenre;
+//    }
+//
+//    public String getArtistId() {
+//        return artistId;
+//    }
+//
+//    public String getArtistName() {
+//        return artistName;
+//    }
+//
+//    public String getArtistGenre() {
+//        return artistGenre;
+//    }
+//
+//    public void setStepCount(String setpCount){
+//        this.stepCount = setpCount;
+//    }
+//
+//    public String getStepCount() {
+//        return stepCount;
+//    }
+//}
