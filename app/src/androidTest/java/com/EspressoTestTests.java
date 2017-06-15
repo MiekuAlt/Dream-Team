2c6034c02a1a6fb4c1152066018d6b4d6e46c0c8
MuffinMauler <NathanRobertson@dal.ca>
2017-06-15 18:47:10 -0300

    Implemented working dummy tests for espresso. Will probably break Travis.

---
+++ app/src/androidTest/java/com/EspressoTestTests.java
@@ -0,0 +1,48 @@
+package com;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.wolkabout.hexiwear.R;
+import com.wolkabout.hexiwear.StepCountActivity;
+import com.wolkabout.hexiwear.TempNav;
+import com.wolkabout.hexiwear.activity.MainActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class EspressoTestTests {
+
+    /*
+        Rules need to be changed based on the functionality you need. Keep this in mind.
+     */
+    @Rule
+    public ActivityTestRule<TempNav> mActivityRule = new IntentsTestRule<TempNav>(
+            TempNav.class);
+
+    @Test
+    public void opensActivity() { //checks if step counter activity is opened when corresponding button clicked
+        onView(withId(R.id.stepCountBut)).perform(click());
+        intended(hasComponent(StepCountActivity.class.getName()));
+    }
+
+    @Test
+    public void opensFragment() {
+        //maybe later
+    }
+
+}
