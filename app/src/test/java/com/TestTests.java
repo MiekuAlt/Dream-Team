222b64e35de5b257e0d0840a9db51c393e7f78c3
MuffinMauler <NathanRobertson@dal.ca>
2017-06-15 16:46:03 -0300

    Unit test example

--- app/src/test/java/com/TestTests.java
+++ app/src/test/java/com/TestTests.java
@@ -4,5 +4,14 @@ package com;
  * Created by Nathan on 2017-06-15.
  */
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TestTests {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
 }
