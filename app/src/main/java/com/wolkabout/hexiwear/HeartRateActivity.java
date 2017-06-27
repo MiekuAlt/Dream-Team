9444de1cd6597a4b4c33e6dcbf837d01eaf807a4
Michael <m.altair@live.com>
2017-06-27 15:28:51 -0300

    Discovered an error in the Max and Min Heart rate range code
    Commented this out until the author can correct it

--- app/src/main/java/com/wolkabout/hexiwear/HeartRateActivity.java
+++ app/src/main/java/com/wolkabout/hexiwear/HeartRateActivity.java
@@ -1,7 +1,6 @@
 package com.wolkabout.hexiwear;
 
 import android.os.Bundle;
-
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -10,8 +9,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.wolkabout.hexiwear.activity.MinHeartRange;
-import com.wolkabout.hexiwear.activity.MaxHeartRange;
 import com.wolkabout.hexiwear.activity.HeartRate;
 
 
@@ -19,8 +16,8 @@ public class HeartRateActivity extends AppCompatActivity {
     TextView heartText;
 
     DatabaseReference databaseHeartRate;
-    DatabaseReference databaseMinHeartRange;
-    DatabaseReference databaseMaxHeartRange;
+    //DatabaseReference databaseMinHeartRange;
+    //DatabaseReference databaseMaxHeartRange;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +26,8 @@ public class HeartRateActivity extends AppCompatActivity {
         
         // Initializing the database
         databaseHeartRate = FirebaseDatabase.getInstance().getReference("HeartRate");
-        databaseMinHeartRange = FirebaseDatabase.getInstance().getReference("MinHeartRange");
-        databaseMaxHeartRange = FirebaseDatabase.getInstance().getReference("MaxHeartRange");
+        //databaseMinHeartRange = FirebaseDatabase.getInstance().getReference("MinHeartRange");
+        //databaseMaxHeartRange = FirebaseDatabase.getInstance().getReference("MaxHeartRange");
 
     }
 
@@ -58,44 +55,44 @@ public class HeartRateActivity extends AppCompatActivity {
                 System.out.println("The read failed: " + databaseError.getCode());
             }
         });
-        databaseMinHeartRange.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                // Grabbing the data from Firebase
-                dataSnapshot.getChildren();
-                MinHeartRange minHeartRange = dataSnapshot.getValue(MinHeartRange.class);
-
-                // Posting the data to appear on the GUI
-                TextView textView = (TextView) findViewById(R.id.minRange);
-                String output = minHeartRange.getMinHeartRange();
-                textView.setText(output);
-
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getCode());
-            }
-        });
-        databaseMaxHeartRange.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                // Grabbing the data from Firebase
-                dataSnapshot.getChildren();
-                MaxHeartRange maxHeartRange = dataSnapshot.getValue(MaxHeartRange.class);
-
-                // Posting the data to appear on the GUI
-                TextView textView = (TextView) findViewById(R.id.maxRange);
-                String output = maxHeartRange.getMaxHeartRange();
-                textView.setText(output);
-
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getCode());
-            }
-        });
+//        databaseMinHeartRange.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//
+//                // Grabbing the data from Firebase
+//                dataSnapshot.getChildren();
+//                MinHeartRange minHeartRange = dataSnapshot.getValue(MinHeartRange.class);
+//
+//                // Posting the data to appear on the GUI
+//                TextView textView = (TextView) findViewById(R.id.minRange);
+//                String output = minHeartRange.getMinHeartRange();
+//                textView.setText(output);
+//
+//            }
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                System.out.println("The read failed: " + databaseError.getCode());
+//            }
+//        });
+//        databaseMaxHeartRange.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//
+//                // Grabbing the data from Firebase
+//                dataSnapshot.getChildren();
+//                MaxHeartRange maxHeartRange = dataSnapshot.getValue(MaxHeartRange.class);
+//
+//                // Posting the data to appear on the GUI
+//                TextView textView = (TextView) findViewById(R.id.maxRange);
+//                String output = maxHeartRange.getMaxHeartRange();
+//                textView.setText(output);
+//
+//            }
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                System.out.println("The read failed: " + databaseError.getCode());
+//            }
+//        });
 
     }
 }
