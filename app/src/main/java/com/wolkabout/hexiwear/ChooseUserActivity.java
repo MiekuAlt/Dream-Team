ff3a505d5b1cbfb304e637d70f0cd130c4d5f425
Michael <m.altair@live.com>
2017-06-29 00:21:35 -0300

    [REFACTORED] Globals class (MA, CS)

--- app/src/main/java/com/wolkabout/hexiwear/ChooseUserActivity.java
+++ app/src/main/java/com/wolkabout/hexiwear/ChooseUserActivity.java
@@ -8,8 +8,6 @@ import android.widget.RadioButton;
 
 public class ChooseUserActivity extends AppCompatActivity {
 
-    //Globals g = Globals.getInstance();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
