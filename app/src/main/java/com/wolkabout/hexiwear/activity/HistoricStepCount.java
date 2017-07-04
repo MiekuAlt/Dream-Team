2cb81ccabced47d56aa4da2a645a97b94e35b2b1
Unknown <evan.cruz.1993@gmail.com>
2017-07-04 00:32:24 -0300

    Step Count Graphing and Avg (EC)
    Added a graph to show the historical data on step count. Pulls the data from FireBase and plots the points based on the end of day step amount. Also added a weekly average that shows how many steps are taken on average for the past 7 days.

---
+++ app/src/main/java/com/wolkabout/hexiwear/activity/HistoricStepCount.java
@@ -0,0 +1,17 @@
+package com.wolkabout.hexiwear.activity;
+
+/**
+ * Created by Evan on 2017-07-03.
+ */
+
+public class HistoricStepCount {
+    public String historicStepCount;
+    public HistoricStepCount() {}
+    public HistoricStepCount(String historicStepCount){
+        this.historicStepCount = historicStepCount;
+    }
+    public void setHistoricStepCount(String historicStepCount){
+        this.historicStepCount = historicStepCount;
+    }
+    public String getHistoricStepCount() {return historicStepCount; }
+}
